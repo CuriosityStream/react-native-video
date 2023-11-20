dcf0be40d169fdf38cfb86bc55fe20d59097d302
tiplefbxx <maxim.pozichenko@curiositystream.com>
2023-11-20 18:19:49 +0200

    another try

--- react-native-video-extension.d.ts
+++ react-native-video-extension.d.ts
@@ -1,18 +1,13 @@
-// Unused import - only used to make this file a module (otherwise declare global won't work)
-import React from "react";
 
-// Extend HTMLImageElement to support extended module
+
 declare module "react-native-video" {
-    namespace NowPlayingManager {
-        //@ts-ignore
-        interface NowPlayingModuleInferface {
+    export interface NowPlayingManager {
             setNowPlaying: (info: {artwork?: string, duration?: number, title?: string, externalContentID?: string }) => void;
             updatePlayback: (info: {speed?: number, state?: string, elapsedTime?: number }) => void;
             resetNowPlaying: () => void;
         }
-        //@ts-ignore
-        interface NowPlayingType {
+
+    export interface NowPlayingType {
             [key: string]: string
         }
-    }
 }
