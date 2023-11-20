633b0df244e9e315ad4b3cf926309d7b4df413af
tiplefbxx <maxim.pozichenko@curiositystream.com>
2023-11-20 19:50:04 +0200

    new typo

--- NowPlayingType.js
+++ NowPlayingType.js
@@ -1,10 +1,8 @@
-import { NativeModules } from 'react-native'
-const NowPlayingManager = NativeModules.NowPlayingManager
 
-const STATE_PLAYING =  NowPlayingManager.STATE_PLAYING
-const STATE_PAUSED = NowPlayingManager.STATE_PAUSED
-const STATE_ERROR =  NowPlayingManager.STATE_ERROR
-const STATE_STOPPED =  NowPlayingManager.STATE_STOPPED
+const STATE_PLAYING =  'STATE_PLAYING'
+const STATE_PAUSED = 'STATE_PAUSED'
+const STATE_ERROR =  'STATE_ERROR'
+const STATE_STOPPED =  'STATE_STOPPED'
 
 export default {
   STATE_PLAYING,
